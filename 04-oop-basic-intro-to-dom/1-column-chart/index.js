0b2dd421f96a4e013d10e8c24bc64a6dbb66f336
Aleksandr Panarin <panarin0290@gmail.com>
2022-06-26 23:52:30 +0300

    Начало реализации

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,39 @@
 export default class ColumnChart {
-
+    constructor(params){
+        this.link = params.link || '#'
+        this.data = params.data || []
+        this.label = params.label || 'Main'
+        this.value = params.value || 0
+        this.formatHeading = params.formatHeading || function (data){};
+        this.render()
+    }
+    createChartContainer(child){
+        const container = document.createElement('div')
+        container.innerHTML = `
+        <div class="column-chart" style="--chart-height: 50">
+        ${child || ''} 
+        </div>`
+        return container
+    }
+    createChartHead(){
+        const head = document.createElement('div')
+        head.innerHTML = `<div class="column-chart__title">
+        Total orders
+        <a href="/sales" class="column-chart__link">View all</a>
+      </div>`
+      return head.firstChild.innerHTML
+    }
+    createChartBody(){
+        const body = document.createElement('div')
+        body.innerHTML = `<div class="column-chart__container">
+        <div data-element="header" class="column-chart__header">344</div>
+        <div data-element="body" class="column-chart__chart">
+          ${this.data.map(value => `<div style="--value: ${value}" data-tooltip="${value}%"></div>`).join('')}
+        </div>
+      </div>`
+      return body.firstChild.innerHTML
+    }
+    render(){
+        this.element = this.createChartContainer(this.createChartHead() + this.createChartBody())
+    }
 }
