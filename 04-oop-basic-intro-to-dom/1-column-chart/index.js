1d8854fabc776ffe44f06c0f06e1e5ecbde74876
Aleksandr Panarin <panarin0290@gmail.com>
2022-06-29 23:01:25 +0300

    внесение корректировок решения

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,39 +1,55 @@
 export default class ColumnChart {
     constructor(params){
-        this.link = params.link || '#'
-        this.data = params.data || []
-        this.label = params.label || 'Main'
-        this.value = params.value || 0
-        this.formatHeading = params.formatHeading || function (data){};
+        let {link, data, label, value, formatHeading} = params || {}
+        this.link = link || '#'
+        this.data = data || []
+        this.label = label || 'Main'
+        this.value = value || 0
+        this.chartHeight = 50
+        this.formatHeading = formatHeading || function (val){return val};
         this.render()
     }
     createChartContainer(child){
         const container = document.createElement('div')
+        container.className = this.data.length === 0 ? "column-chart_loading" : ''
         container.innerHTML = `
-        <div class="column-chart" style="--chart-height: 50">
-        ${child || ''} 
+        <div class="column-chart" style="--chart-height: ${this.chartHeight}">
+          ${child || ''} 
         </div>`
         return container
     }
     createChartHead(){
-        const head = document.createElement('div')
-        head.innerHTML = `<div class="column-chart__title">
-        Total orders
-        <a href="/sales" class="column-chart__link">View all</a>
+      return `
+      <div class="column-chart__title">Total ${this.label}
+      <a href="/${this.label}" class="column-chart__link">View all</a>
       </div>`
-      return head.firstChild.innerHTML
     }
     createChartBody(){
-        const body = document.createElement('div')
-        body.innerHTML = `<div class="column-chart__container">
-        <div data-element="header" class="column-chart__header">344</div>
+        const props = val => {
+          const maxValue = Math.max(...this.data)
+          const value = this.chartHeight/maxValue*val
+          return {
+            percent: (val / maxValue * 100).toFixed(0) + '%',
+            value: Math.floor(value)}
+        }
+        
+        return `<div class="column-chart__container">
+        <div data-element="header" class="column-chart__header">${this.formatHeading(this.value)}</div>
         <div data-element="body" class="column-chart__chart">
-          ${this.data.map(value => `<div style="--value: ${value}" data-tooltip="${value}%"></div>`).join('')}
+          ${this.data.map(val => `<div style="--value: ${props(val).value}" data-tooltip="${props(val).percent}"></div>`).join('')}
         </div>
       </div>`
-      return body.firstChild.innerHTML
     }
     render(){
         this.element = this.createChartContainer(this.createChartHead() + this.createChartBody())
+    }
+    destroy(){}
+    remove(){
+      this.element.remove()
+    }
+    update(data){
+      this.data = data
+      const body = document.querySelector('.column-chart__container')
+      body.innerHTML = this.createChartBody()
     }
 }
